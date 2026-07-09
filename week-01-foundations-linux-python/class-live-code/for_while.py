2c65334251be6c6a7d8f292c496a04af7dc923b7
Shakoor Afridi <shakoorkhan031894@gmail.com>
2026-07-09 16:50:16 +0500

    home

---
+++ week-01-foundations-linux-python/class-live-code/for_while.py
@@ -0,0 +1,7 @@
+number = []
+for number  in range(51):
+    if number % 2 == 0:
+
+        print(i)
+    
+    
--- week-01-foundations-linux-python/class-live-code/temp.py
+++ week-01-foundations-linux-python/class-live-code/temp.py
@@ -1,4 +1,4 @@
-num1 = int(input("enter first number : "))
-num2 = int(input("enter second number : "))
-
-print(f"Addition of {num1} and {num2} = ", num1+num2 )
+number = 1
+while number <= 10:
+    print(number)
+    number += 1
